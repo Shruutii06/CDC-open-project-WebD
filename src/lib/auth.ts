3e8ca877a0deee5c700d8b97439e615f4f8020d2
Shruutii06 <shruti050406@gmail.com>
2026-01-01 13:40:12 +0530

    reduced loading time

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,11 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET: Secret = process.env.JWT_SECRET as Secret;
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in environment variables");
 }
 
-/**
- * Verifies JWT token
- * Throws error if invalid or expired
- */
 export function verifyToken(token: string): JwtPayload {
   try {
     return jwt.verify(token, JWT_SECRET) as JwtPayload;
